1699afa511be1f7fa422ff70072ceaf54de8f7cf
shubham99298 <83624288+shubham99298@users.noreply.github.com>
2024-09-21 17:48:58 +0530

    learn JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+// import { Component } from "react";
+// class App extends Component{
+//   render(){
+//     return <h1>Hello Class Based Component</h1>;
+//   }
+// }
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+
+import Add from "./components/Add";
+const myName='shubham';
+const mul=(a,b)=> a*b;
+const myclass='Div';
+const numbers=[1,2,3,4,5];
+
+function App(){
+  return(
+    <>
+    
+     <h2>{myName}</h2>
+     <h3>{mul(3,4)}</h3>
+     <p className={myclass}>My friends List:{['Alex','John']}</p>
+     <Add/>
+    
+     {numbers.map((number)=>(
+       <ul key={Math.random}>
+        <li>{number}</li>
+       </ul>
+     ))}
+    </>
+   
+  )
 }
-
 export default App;
